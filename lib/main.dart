af02fc02ea17f007d18a3dda1942ef53472b9ce5
Riya Suntwal <41776149+carrisunrio@users.noreply.github.com>
2020-07-08 17:11:49 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,107 +2,144 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:line_icons/line_icons.dart';
 
-void main() => runApp(MaterialApp(
-    title: "GNav",
-    theme: ThemeData(
-      primaryColor: Colors.grey[800],
-    ),
-    home: BottomNavBar()));
+void main() => runApp(TabPage());
 
-class BottomNavBar extends StatefulWidget {
+class TabPage extends StatefulWidget {
   @override
-  _BottomNavBarState createState() => _BottomNavBarState();
+  _TabPageState createState() => _TabPageState();
 }
 
-class _BottomNavBarState extends State<BottomNavBar> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Likes',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Search',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Profile',
-      style: optionStyle,
-    ),
+class _TabPageState extends State<TabPage> {
+  int selectedIndex = 0;
+
+  PageController controller = PageController();
+
+  List<GButton> tabs = new List();
+  List<Color> colors = [
+    Colors.purple,
+    Colors.pink,
+    Colors.amber[600],
+    Colors.teal
   ];
 
   @override
+  void initState() {
+    super.initState();
+
+    var padding = EdgeInsets.symmetric(horizontal: 18, vertical: 5);
+    double gap = 10;
+
+    tabs.add(GButton(
+      gap: gap,
+      iconActiveColor: Colors.purple,
+      iconColor: Colors.black,
+      textColor: Colors.purple,
+      backgroundColor: Colors.purple.withOpacity(.2),
+      iconSize: 24,
+      padding: padding,
+      icon: LineIcons.home,
+      // textStyle: t.textStyle,
+      text: 'Home',
+    ));
+
+    tabs.add(GButton(
+      gap: gap,
+      iconActiveColor: Colors.pink,
+      iconColor: Colors.black,
+      textColor: Colors.pink,
+      backgroundColor: Colors.pink.withOpacity(.2),
+      iconSize: 24,
+      padding: padding,
+      icon: LineIcons.heart_o,
+      // textStyle: t.textStyle,
+      text: 'Likes',
+    ));
+
+    tabs.add(GButton(
+      gap: gap,
+      iconActiveColor: Colors.amber[600],
+      iconColor: Colors.black,
+      textColor: Colors.amber[600],
+      backgroundColor: Colors.amber[600].withOpacity(.2),
+      iconSize: 24,
+      padding: padding,
+      icon: LineIcons.search,
+      // textStyle: t.textStyle,
+      text: 'Search',
+    ));
+
+    tabs.add(GButton(
+      gap: gap,
+      iconActiveColor: Colors.teal,
+      iconColor: Colors.black,
+      textColor: Colors.teal,
+      backgroundColor: Colors.teal.withOpacity(.2),
+      iconSize: 24,
+      padding: padding,
+      icon: LineIcons.user,
+      // textStyle: t.textStyle,
+      text: 'Profile',
+    ));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: const Text('Bottom Navigation Bar')),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: 
-      SafeArea(
-        child: Container(
-          decoration: 
-          BoxDecoration(
-            color: Colors.white, 
-            boxShadow: [
-            BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
-          ]),
-          child: Padding(
-              padding: const EdgeInsets.all(8.0),
+    return MaterialApp(
+      home: Scaffold(
+        extendBody: true,
+        appBar: AppBar(
+          brightness: Brightness.dark,
+          title: Text(
+            'GoogleNavBar',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        body: PageView.builder(
+          onPageChanged: (page) {
+            setState(() {
+              selectedIndex = page;
+            });
+          },
+          controller: controller,
+          itemBuilder: (context, position) {
+            return Container(
+              color: colors[position],
+            );
+          },
+          itemCount: tabs.length, // Can be null
+        ),
+        // backgroundColor: Colors.green,
+        // body: Container(color: Colors.red,),
+        bottomNavigationBar: SafeArea(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(100)),
+                boxShadow: [
+                  BoxShadow(
+                      spreadRadius: -10,
+                      blurRadius: 60,
+                      color: Colors.black.withOpacity(.20),
+                      offset: Offset(0, 15))
+                ]),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 5),
               child: GNav(
-                  gap: 8,
-                  activeColor: Colors.white,
-                  iconSize: 30,
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  duration: Duration(milliseconds: 500),
-                  tabBackgroundColor: Colors.grey[800].withOpacity(.9),
-                  tabs: [
-                    GButton(
-                      backgroundColor: Colors.yellow,
-                      textColor: Colors.black,
-                      iconActiveColor: Colors.black,
-                      icon: LineIcons.home,
-                      text: 'Home',
-                    ),
-                    GButton(
-                      backgroundColor: Colors.pink[300],
-                      textColor: Colors.black,
-                      iconActiveColor: Colors.black,
-                      icon: LineIcons.heart_o,
-                      text: 'Likes',
-                    ),
-                    GButton(
-                      backgroundColor: Colors.greenAccent,
-                      textColor: Colors.black,
-                      iconActiveColor: Colors.black,
-                      icon: LineIcons.search,
-                      text: 'Search',
-                    ),
-                    GButton(
-                      backgroundColor: Colors.blueAccent,
-                      textColor: Colors.black,
-                      iconActiveColor: Colors.black,
-                      icon: LineIcons.user,
-                      text: 'Profile',
-                    ),
-                  ],
-                  selectedIndex: _selectedIndex,
+                  tabs: tabs,
+                  selectedIndex: selectedIndex,
                   onTabChange: (index) {
+                    print(index);
                     setState(() {
-                      _selectedIndex = index;
+                      selectedIndex = index;
                     });
+                    controller.jumpToPage(index);
                   }),
             ),
+          ),
         ),
       ),
-      );
+    );
   }
 }
